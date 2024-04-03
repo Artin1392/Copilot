3275ce6faa4a3268b8a923112b067125081827ed
Artin Fardjzade Mevaloo <artinmevaloo@gmail.com>
2024-04-03 07:54:32 +0330

    Some codes

---
+++ copilot.py
@@ -0,0 +1,42 @@
+# While key
+app = True
+
+# Introduce
+print("Hello! I am Copilot, your friendly AI assistant.")
+print('What can I do for you?')
+
+# Functions
+
+# Write a song
+def was():
+    song = "I'm a great singer.\nBut what about you?\nYeah,Yeah you are a bad singer.\n Ha, Ha, Ha, HaHaHaHa"
+    print(song)
+
+# Echo
+def Echo():
+    text = input('Just say something:\n')
+    print(text)
+
+# Exit
+def Exit():
+    app = False
+    return app
+    
+
+
+# While loop
+while(app):
+    inp = input()
+    try: 
+        if inp.lower() == "write a song":
+            was()
+
+        elif inp.lower() == "echo":
+            Echo()
+
+        elif inp.lower() == "exit":
+            Exit()
+    
+    except:
+        print("Sorry, an error occured\n")
+
